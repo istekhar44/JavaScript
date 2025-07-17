e3af259a9297708c1fe876bd829f016234d76bf6
istekhar44 <67890istekharalam@gmail.com>
2025-07-17 22:14:39 +0530

    JavaScript

---
+++ Varible/script.js
@@ -0,0 +1,91 @@
+console.log("hellow");
+console.log('hellow');
+console.log("My First Program in the javaScript");
+
+let r = 20;
+let pi = 13.4;
+let area = r*r
+console.log(area);
+
+let age = 45;
+console.log(age);
+
+isFollow = true ;
+console.log(isFollow);
+
+isHero = false;
+console.log(isHero);
+
+MdIstekhar = 45;
+console.log(MdIstekhar);   //It is dynamically tiped language.
+
+
+//  This is=> = assingment operator .
+
+//Varible are the case Sensitive (a) and the (A) are the differnt .
+
+const a = 45;   //Always define the varible in the cont
+console.log(a);
+
+
+
+//There are the different type of the data type
+//Premitive Data Type = 7 data type
+
+let rollNo = 34;
+console.log(rollNo);  //use the Type to no the data type => typeof varibleName
+
+let x = null;    // null <=is a Object it meaning Absence of OBJECT
+
+
+// None Premitive data Type 
+// Array 
+
+//Object are the created by use of => Const.
+//Cration of the Object in the Java Script 
+const student ={
+    name : "Md Istekhar Alam",
+    height : 34 ,
+    fullname : "Mohit",
+
+
+};
+//To Access the the class Member  just only type class name , eg => sdudent
+//To Acces the single member of the class 
+student["name"];
+console.log(student["name"]);
+
+//Make the card Prodect javascript
+const product ={
+    title : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+};
+console.log(product);
+let z = "abcd"+123;
+console.log(z);
+console.log("hellow ji kase ");
+
+
+
+
+
+//Make the profile
+const profile ={
+    username : "@istekhar.ig",
+    isFollow: true ,
+    followers: 234,
+    bio: "Follow for more exciting concept of javaScript",
+
+};
+//print the whole class
+console.log(profile);
+
+//Access the single member of the class 
+//console.log(profile.const["username"]); //wrong stntax
+
+//Print the type of the class  member
+console.log(typeof profile["username"])
+
+
