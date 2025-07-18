e47748b0e6afa8a7fa38d7176d5a78060b625afd
istekhar44 <67890istekharalam@gmail.com>
2025-07-18 15:58:33 +0530

    conditon

--- Operators/operators.js
+++ Operators/operators.js
@@ -1,1 +1,50 @@
-console.log("Operators")
+console.log("Operators");
+let a = 23;
+let b = 65;
+console.log("a+b =",a+b);
+//operand => a
+//operator => + , - , * 
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a*b =",a*b);
+
+//Modulus to find the reminder
+console.log("a%b =",a%b);
+
+//Power Operator 
+console.log("a**b =",a**b);
+
+//Logical And opertor &&
+let x= 5;
+let y= 6;
+let cond1 = x < y;  //true 
+let cond2 = x == y; //false 
+console.log("cond1 && cond2 = ", cond1 && cond2);  //final ans is False
+
+
+
+
+ // website Dark and the light mode 
+ let color;
+ let mode = "dark";
+
+ if(mode === "dark"){
+     color= "Dark";
+ }else{
+   color="Write";
+ }
+ console.log(color)
+
+
+
+//make the voter conditon in the 
+let age = 34;
+if(age > 18){
+    console.log("You Can Vote :)");
+}
+else{
+    console.log("Your Cant Vote ");
+}
+
+
+//
