0346f27aa3c50c22d1113d5b97936953d763352a
istekhar44 <67890istekharalam@gmail.com>
2025-07-18 16:41:17 +0530

    loop

--- Operators/operators.js
+++ Operators/operators.js
@@ -47,4 +47,93 @@ else{
 }
 
 
-//
+//Multiple Condition operatoru
+let ageing =33;
+
+if(ageing < 18){
+    console.log("Child");
+}
+else if(ageing < 55){
+    console.log("Adult");
+}
+else{
+    console.log("Old Men")
+}
+
+
+//Write the multiple condition to change the Mode 
+
+
+
+
+
+
+
+
+//What is the ternerory operator
+let saal = 12;
+let result = saal > 18 ?  "Adult" : "Not Adult";    //This is Compact if else .
+console.log(result);
+
+eg=>  condition ? "print value 1" : "Print Value 2";
+
+
+
+
+
+
+                    //  IMPORTANT
+//Alert Messages
+// alert("hellow");  //Create the one time message
+
+//Alert want take the message 
+// prompt("Number");
+
+
+
+
+
+//Take the input alert and Check the no is Divisible of 5
+let num = prompt("Enter the Number ");
+if(num%5==0){
+    console.log(num,"This Number  is division of 5");
+}
+else{
+    console.log(num,"This Number  is not divisble of 5 ")
+}
+
+
+
+
+
+//Write the code to print the grade  A ,B , C , D , F?
+let score = 8;
+if(score >= 90 && score <= 100){
+    console.log("First Division");
+} 
+else if (score >= 70 && score<= 80);{
+    console.log("Second Division");
+}
+// else if (score >=0 && score<=10){
+//     console.log("fail");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
