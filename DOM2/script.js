e9e860fbe0fc2f330275720d240bef80cd84f8fc
istekhar44 <67890istekharalam@gmail.com>
2025-08-10 11:44:37 +0530

    commit

---
+++ DOM2/script.js
@@ -0,0 +1,10 @@
+document.querySelector("div").lastChild;
+
+let heading1 = document.querySelector("heading1");
+heading1.innerText = "hellow";
+
+let heading2 = document.querySelector("heading2");
+heading2.textContent = "hellow World";
+
+
+
