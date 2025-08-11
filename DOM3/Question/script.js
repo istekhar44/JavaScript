0e0bc141a9a7b1f86a191a6d8d54de390872db64
istekhar44 <67890istekharalam@gmail.com>
2025-08-11 19:49:17 +0530

    Event

--- DOM3/Question/script.js
+++ DOM3/Question/script.js
@@ -1,4 +1,37 @@
 //Create a new butoon. Give it a text "click me" , background color of red & text color is white .
 //Insert the button as the first element inside the body tag.
+                    
 
- 
+        //    Button1
+  let newBtn = document.createElement("Button");   
+  newBtn.innerText = "Click Me!";
+
+  newBtn.style.background= "red";
+  newBtn.style.color = "white";
+
+  document.querySelector("body").prepend(newBtn);
+
+
+
+
+               //Button2
+  let hellow = document.createElement("Button");
+  hellow.innerText ="ContactUS";
+  hellow.style.background = "black";
+  hellow.style.color = "white"
+
+  document.querySelector("body").append(hellow);
+
+
+
+//   Question2 Create a <p> tag in html, give it clsss & some styling .
+//   Now Create a new class in CSS and try to append this class to the <p> Element.
+//   Did yoy notice ,how you overrite the class name when you add a new one?
+//   Solve this problem ClassList . 
+
+
+
+
+  
+
+
