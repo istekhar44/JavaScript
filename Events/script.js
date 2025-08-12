a1d12e0d85732f3587a4f08148e6e6aae45209aa
istekhar44 <67890istekharalam@gmail.com>
2025-08-12 19:30:37 +0530

    Event

--- Events/script.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById('eventBtn').addEventListener('click', function() {
-//     document.getElementById('output').textContent = 'Button was clicked! Event triggered.';
-// });
-
-
-
-
