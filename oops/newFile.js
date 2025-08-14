fe5c0b9219c6859e92113cf951ae2bdfe5f3efc0
istekhar44 <67890istekharalam@gmail.com>
2025-08-14 20:02:49 +0530

    commit the Script

---
+++ oops/newFile.js
@@ -0,0 +1,4 @@
+const { employee } = require("./script");
+
+karanArjun3.__proto__ = employee; // Make the multiple employee whithout creating without writing the multiple fun
+
--- oops/script.js
+++ oops/script.js
@@ -1,10 +1,47 @@
-//Classes and the objects
-const student = {
-    namme : Rohit,
+let student = {
     age : 23,
     class: 10,
     printMarks: function(){
         console.log("hellow", this.marks);
     },
+    name : "hellow",
      
+};
+
+let arr = ['apple','orange','banana'];   //Array 1 Dimension
+
+arr.push("litchi");  //write the command on the console window to push the element in the array
+
+
+              //PotoType in the calsses and function
+              //PotoType are used to create the clasees
+
+
+const employee = {
+    calculateTax1(){
+        console.log("tax rate is 10%");
+    },
+    calculateTax2(){
+        console.log("tax rate is 20%");
+    },
+
+
+};
+
+const karanArjun ={       
+    salary : 5000,
+    calTax(){
+        console.log("tax rate is 20%");
+    },
+};
+
+karanArjun.__proto__ = employee;    //Create the prototype using the classes function 
+
+
+         //Classes in the opps
+
+
+
+class ToyotaCar{
+
 };
