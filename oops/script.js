82883bf3b7929d728e787a42fcd959b662e9e453
istekhar44 <67890istekharalam@gmail.com>
2025-08-14 19:11:32 +0530

    initlize the files

---
+++ oops/script.js
@@ -0,0 +1,10 @@
+//Classes and the objects
+const student = {
+    namme : Rohit,
+    age : 23,
+    class: 10,
+    printMarks: function(){
+        console.log("hellow", this.marks);
+    },
+     
+};
