d1f7d0b295c48610328e66b3f155e7ea81243591
istekhar44 <67890istekharalam@gmail.com>
2025-08-15 18:56:42 +0530

    commit the class

--- oops/script.js
+++ oops/script.js
@@ -1,4 +1,4 @@
-let student = {
+let student = {       //creating the multiple objects
     age : 23,
     class: 10,
     printMarks: function(){
@@ -14,14 +14,14 @@ arr.push("litchi");  //write the command on the console window to push the eleme
 
 
               //PotoType in the calsses and function
-              //PotoType are used to create the clasees
+              //PotoType are used to create the Objects
 
 
 const employee = {
     calculateTax1(){
         console.log("tax rate is 10%");
     },
-    calculateTax2(){
+    calTax(){
         console.log("tax rate is 20%");
     },
 
@@ -43,5 +43,81 @@ karanArjun.__proto__ = employee;    //Create the prototype using the classes fun
 
 
 class ToyotaCar{
+    constructor(){
+        console.log("creating new boject");
+    }
+
+
+    start(){
+        console.log("start");
+
+    }
+
+    stop(){
+        console.log("stop");
+    }
+
+    setBrand(brand){
+        this.brandName = brand;
+
+    }
 
 };
+
+fortuner = new ToyotaCar();
+laxax = new ToyotaCar();
+fortuner.setBrand("BMW");  //Set the brand name in the class
+
+
+                   //INHERITANCE
+class parent{
+    karan(){
+        console.log("hellow");
+    }
+    
+}  
+
+class mohit extends parent{
+
+    mohit(){
+        console.log("helloow");
+    }
+    hellow(){
+        console.log("mohit is the king ");
+    }    
+}
+
+
+karan = new mohit();
+console.log(karan);
+
+let rawat = new mohit();
+
+
+         //Creating the 2 class
+
+class person{
+    eat(){
+        console.log("Eat");
+    }
+    sleep(){
+        console.log("sleep");
+    }
+} 
+
+class Engineer extends person{
+    work(){
+        console.log("Engineer can work, build Something");
+    }
+}
+
+class doctor extends Engineer{
+    operation(){
+        console.log("operation");
+    }
+}
+
+let name = new doctor;
+console.log(name);
+
+
