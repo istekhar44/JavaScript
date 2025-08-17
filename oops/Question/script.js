90a2f7b3daa341e6f0c7ed1e2f0b37bdedf27fb5
istekhar44 <67890istekharalam@gmail.com>
2025-08-17 20:50:20 +0530

    Error handdling

--- oops/Question/script.js
+++ oops/Question/script.js
@@ -12,14 +12,59 @@ class User {
     }
 }
 
+class Admin extends USer {
+    constructor(name , email){   
+        super(name ,email);    //When call the parent properties  USe the super keyword to access the data   
+
+    }
+    editData(){
+        DATA = "some new value";
+    }
+}
 let Student1 = new User( "Istekhar","mdistekharalam@gmail.com");
 let Student2 = new User();
 let Student3 = new User();
-let teacher1 = new User();
+let teacher1 = new User("Dean","dean@gmail.com");
+
+let admin1 = new Admin();
 
 
 
 
 //Question2: Create a new class called admin which inherits form User . Add a new method called  
 //editData to Adimin that allow it to edit website data
+
+
+
+//Error Handling 
+let a = 5;
+let b = 10 ;
+console.log("a=  " , a);
+console.log("a + b= " , a+b);
+console.log("a - b = " , a-b);
+console.log("a * b = " , a - b);   //Erroe in this line 
+try {
+    console.log("a * b = " , a - b);
+
+} catch( error){
+    console.log(error);  //Print the Error
+}
+
+console.log("a= " , a);
+console.log("a= " , a);
+
+// Syntax
+// try{
+//     ...normal code 
+
+// } catch (Error){  //Error is object
+
+//     ...handling Error
+
+// }
+
+
+
+
+
 
