4565e3f7b69056a2580b935fab27b9aa32e4c98b
istekhar44 <67890istekharalam@gmail.com>
2025-08-19 18:16:19 +0530

    commit

---
+++ API/script.js
@@ -0,0 +1,6 @@
+//Fetch API
+//The Fetch API provide an interface for fetching (Sending / Reciving) Resources
+//It use Request and Response objects.
+//The fetch() method is used to fetch a resources (data).
+
+const URL = 
